ba153fdbc06241b791d7baf7cd663d06242958ae
David Walker <github-dwalker@vorteon.com>
2022-09-08 00:32:34 -0400

    new

--- Utilities/ConvertJson/Program.cs
+++ Utilities/ConvertJson/Program.cs
@@ -1,12 +1,30 @@
 ﻿using GlobExpressions;
+using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json.Linq;
 using System.Text;
 
+Console.WriteLine("Processing Json Language Resource files");
+
+var config = new ConfigurationBuilder()
+       .SetBasePath(Directory.GetCurrentDirectory())
+       .AddJsonFile("appsettings.json", optional:true, reloadOnChange: true)
+       .Build();
+
 var currentFolder = Environment.CurrentDirectory;
-Console.WriteLine("Creating .restext files for all json files in current folder.");
+Console.WriteLine("Creating .restext files for all json files matching pattern.");
 Console.WriteLine($"Current Folder: {currentFolder}");
 
-string[] matchingFiles = Glob.Files(currentFolder, "**/*.json").ToArray();
+string[] matchingFiles = Glob.Files(currentFolder, "**/Languages/*.json").ToArray();
+
+var lineWidth = 40;
+try
+{
+    lineWidth = Console.WindowWidth;
+}
+catch
+{
+    // fallback is 40, above;
+}
 
 foreach (var matchingFile in matchingFiles.Select(mf => Path.GetFullPath(mf, currentFolder)))
 {
@@ -22,15 +40,12 @@ foreach (var matchingFile in matchingFiles.Select(mf => Path.GetFullPath(mf, cur
     var content = File.ReadAllText(matchingFile);
     var jObject = JObject.Parse(content);
 
-    Console.WriteLine($"Parsing {matchingFile} ::");
+    Console.WriteLine($"Parsing {Path.GetFileName(matchingFile)} -> {Path.GetFileName(targetPath)}");
 
-    using (var outputFile = new StreamWriter(targetPath, false, Encoding.UTF8))
+    using var outputFile = new StreamWriter(targetPath, false, Encoding.UTF8);
+
+    foreach (var item in jObject.DescendantsAndSelf().OfType<JValue>())
     {
-        foreach (var item in jObject.DescendantsAndSelf().OfType<JValue>())
-        {
-            outputFile.WriteLine($"{item.Path}={item.Value}");
-        }
+        outputFile.WriteLine($"{item.Path}={item.Value}");
     }
-
-    Console.WriteLine(new string('-', Console.WindowWidth));
 }
